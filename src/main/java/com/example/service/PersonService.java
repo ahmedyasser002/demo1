fc6846962e94f6c263e9328727ad52b5792deccf
Ahmed Yasser <112984769+ahmedyasser002@users.noreply.github.com>
2024-08-26 03:33:38 +0300

    Update PersonService.java new

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,6 +39,13 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
+
+		p = new Person();
+		p.setId("5");
+		p.setAge(22);
+		p.setFirstName("Ahmed");
+		p.setLastName("Yasser");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
